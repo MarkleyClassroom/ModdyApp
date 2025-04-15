2142af4b6930348631d128778753018264a70687
Kirsten Markley <markleyk@otc.edu>
2025-04-15 16:45:48 -0500

    Added a new view

---
+++ ModdyApp/Moods.swift
@@ -0,0 +1,18 @@
+//
+//  Moods.swift
+//  ModdyApp
+//
+//  Created by KIRSTEN Markley on 4/15/25.
+//
+
+import SwiftUI
+
+struct Moods: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Moods()
+}
