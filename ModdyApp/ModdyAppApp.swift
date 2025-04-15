8c4fe40c38997f90315c4f6c37443fbd339f7b5a
Kirsten Markley <markleyk@otc.edu>
2025-04-15 16:25:22 -0500

    Initial Commit

---
+++ ModdyApp/ModdyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModdyAppApp.swift
+//  ModdyApp
+//
+//  Created by KIRSTEN Markley on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ModdyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
